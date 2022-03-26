3cbcc7d648bee94f6095283926bbc6ca76a7ae21
vin1102 <kelvinfebriannugroho@gmail.com>
2022-03-27 04:30:40 +0700

    commit7

---
+++ proses.php
@@ -0,0 +1,31 @@
+<?php
+
+include "model.php";
+if (isset($_POST["submit_simpan"])) {
+    $nim = $_POST["nim"];
+    $nama = $_POST["nama"];
+    $uts = $_POST["uts"];
+    $uas = $_POST["uas"];
+    $tugas = $_POST["tugas"];
+    $model = new Model();
+    $model->insert($nim, $nama, $uts, $uas, $tugas);
+    header("Location:index.php");
+}
+
+if (isset($_POST["submit_edit"])) {
+    $nim = $_POST["nim"];
+    $nama = $_POST["nama"];
+    $uts = $_POST["uts"];
+    $uas = $_POST["uas"];
+    $tugas = $_POST["tugas"];
+    $model = new Model();
+    $model->update($nim, $nama, $uts, $uas, $tugas);
+    header("Location:index.php");
+}
+
+if (isset($_GET["nim"])) {
+    $id = $_GET["nim"];
+    $model = new Model();
+    $model->delete($id);
+    header("Location:index.php");
+}
